4ade28581fe93aba6b0ba1728c8c5192ea29433b
yoshifumi-yamamoto <toyotama.arank.acekiller@gmail.com>
2020-06-15 14:57:05 +0900

    [fix]hello.php修正 ・$GET=>$_GET ・$POST=>$_POST

--- hello.php
+++ hello.php
@@ -1,9 +1,9 @@
 <?php
 
 if(isset($_GET['get_php'])){
-  echo $GET['get_php'];
+  echo $_GET['get_php'];
 }
 
 if(isset($_POST['post_php'])){
-  echo $POST['post_php'];
+  echo $_POST['post_php'];
 }
